183937e72387d0d2725caafd0fec737c0b5700df
Ajay Krishnamurthy <46771241+avkrishnamurthy@users.noreply.github.com>
2022-10-13 13:10:33 -0400

    Getting team data from NBA-sports-reference

---
+++ team-data.py
@@ -0,0 +1,12 @@
+import os
+teams = ["MIA", "WAS", "TOR", "PHI", "ORL", 
+         "NYK", "MIL", "ATL", "IND", "CLE", 
+         "BOS", "NJN", "CHI", "CHA", "DET", 
+         "MEM", "DEN", "DAL", "MIN", "NOH", 
+         "GSW", "OKC", "LAL", "LAC", "PHO", 
+         "POR", "SAC", "SAS", "UTA", "HOU"]
+os.mkdir("data")
+for team in teams:
+    url = f"https://www.basketball-reference.com/teams/{team}/stats_basic_totals.html#stats"
+    nba = pd.read_html(url)
+    data = pd.DataFrame(nba[0]).to_csv(f"data/{team}.csv")
